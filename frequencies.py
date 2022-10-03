168e756180976e9ecb45203d2bdd179f8799b933
Mustafa Mohamed <mustafa.mohamed@kcl.ac.uk>
2022-10-03 16:30:19 +0100

    Exercise 1.2 completed

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
+
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in items:
+        if type(i) != str:
+            n = items.index(i)
+            items[n] = str(i)
+    
+    for i in items:
+        frequencies[i] = items.count(i)
+
     return frequencies
+
+
+print(f"The frequencies of each input are {frequencies(['a', 'a', 'b', 'b', 'b', 'c', '100', 100])}")
